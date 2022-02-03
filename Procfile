c5e56bc9d21442da5b19a5d25dc8acd090b26895
Christopher Bowers <c_bowers@me.com>
2022-02-03 14:44:19 -0500

    Updated Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn doggydoor_django.wsgi
--- doggydoor_django/urls.py
+++ doggydoor_django/urls.py
@@ -19,6 +19,6 @@ from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include(doggydoor.urls)),
+    path('', include('doggydoor.urls')),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework'))
 ]
