a1c9889f74a633f7f55e21ca5c247e0e5bdd240f
annika truong <annikatruongt@gmail.com>
2022-02-03 09:21:08 -0800

    made migrations and createsuperuser and edit admit file.

--- doggydoor/admin.py
+++ doggydoor/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Dog, Breeds, Shelter
 # Register your models here.
+
+admin.site.register(Dog)
+admin.site.register(Breeds)
+admin.site.register(Shelter)
