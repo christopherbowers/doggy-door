9b5c92ce02196e2b5c446878b18dbb0c0d700a29
Christopher Bowers <c_bowers@me.com>
2022-02-03 12:33:22 -0500

    shelter page and shelter card

---
+++ client/src/pages/Shelters.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <ShelterCard
+      :key="shelter.id"
+      v-for="shelter in shelters"
+      :shelter="shelter"
+    />
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+import ShelterCard from '../components/ShelterCard'
+console.log(ShelterCard)
+
+export default {
+  name: 'Shelters',
+  components: {
+    ShelterCard
+  },
+  data: () => ({
+    shelters: []
+  }),
+  mounted() {
+    this.getShelters()
+  },
+  methods: {
+    async getShelters() {
+      const res = await axios.get(`http://localhost:8000/shelters`)
+      this.shelters = res.data
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
--- client/src/router.js
+++ client/src/router.js
@@ -1,10 +1,11 @@
 import VueRouter from  'vue-router'
 import Home from './pages/Home'
-// import Shelters from './pages/Shelters'
+import Shelters from './pages/Shelters'
 // import AllDogs from './pages/AllDogs'
 
 const routes = [
-  {path: '/', component: Home, name: 'Home' }
+  {path: '/', component: Home, name: 'Home' },
+  {path: '/shelters', component: Shelters, name: 'Shelters' },
 ]
 
 export default new VueRouter({ routes, mode: 'history' })
